1314547477ee0be90343eed182a94a2b9724f461
Tony <korobok.avp@gmail.com>
2019-03-19 12:11:30 +0200

    v0.2

---
+++ js/slider1.js
@@ -0,0 +1,13 @@
+
+    $('.sl').slick({
+        dots: true,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        arrows: true,
+        nextArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-angle-double-right" aria-hidden="true"></i>',
+        prevArrow: '<button id="next" type="button" class="btn btn-juliet"><i class="fa fa-angle-double-left" aria-hidden="true"></i>'
+        //appendArrows: $('.my-arrow'),
+        //prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-angle-double-left" aria-hidden="true"></i></button>',
+        //nextArrow: '<button id="next" type="button" class="btn btn-juliet"><i class="fa fa-angle-double-right" aria-hidden="true"></i></button>'
+    });
+ 
