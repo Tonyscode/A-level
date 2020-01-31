8d8c6a62f48df08a877483194615415dfc7095f6
Antonina Mokliak <korobok.avp@gmail.com>
2020-01-31 11:43:04 +0200

    [исправлено отображение слайдера в моб. верс]

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 (function($){
     $(function() {
         $('.menu__icon').on('click', function() {
+            $(".menu__links").slideToggle(500);
             $(this).closest('.menu').toggleClass('menu_state_open');
         });
     });
 })(jQuery);
-
--- js/slider1.js
+++ js/slider1.js
@@ -5,7 +5,35 @@ function SlickInit() {
         autoplaySpeed: 2000,
         arrows: true,
         nextArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-angle-right" aria-hidden="true"></i>',
-        prevArrow: '<button id="next" type="button" class="btn btn-juliet"><i class="fa fa-angle-left" aria-hidden="true"></i>'
+        prevArrow: '<button id="next" type="button" class="btn btn-juliet"><i class="fa fa-angle-left" aria-hidden="true"></i>',
+
+
+        responsive: [
+            {
+                breakpoint: 510,
+                settings: {
+                    autoplay: true,
+                    autoplaySpeed: 2000,
+                    arrows: false,
+                    infinite: true,
+                    fade: true,
+                    speed: 1000,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 470,
+                settings: {
+                    autoplay: true,
+                    autoplaySpeed: 2000,
+                    arrows: false,
+                    infinite: true,
+                    fade: true,
+                    speed: 1000,
+                    dots: false
+                }
+            }
+            ]
         //appendArrows: $('.my-arrow'),
         //prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-angle-double-left" aria-hidden="true"></i></button>',
         //nextArrow: '<button id="next" type="button" class="btn btn-juliet"><i class="fa fa-angle-double-right" aria-hidden="true"></i></button>'
